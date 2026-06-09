b7196c4c4455958759a717109f4890bddd8724b1
Karthik-03901 <quantixlabs41@gmail.com>
2026-06-09 16:24:11 +0530

    fix: disable turbopack to fix lightningcss build error

--- asrivo-website/next.config.ts
+++ asrivo-website/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   output: "standalone",
+  turbopack: false,
   images: {
     remotePatterns: [
       {
@@ -27,5 +27,4 @@ const nextConfig: NextConfig = {
     ];
   },
 };
-
 export default nextConfig;
